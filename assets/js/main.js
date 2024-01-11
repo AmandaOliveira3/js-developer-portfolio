a43812db53ce9e7ec86f18849129d5282702f6d4
AmandaOliveira3 <amanda.oliveira03@unesp.br>
2024-01-11 19:31:03 -0300

    adicionando foto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
 
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
